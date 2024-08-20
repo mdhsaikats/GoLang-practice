6201b54d09b827b9e04b7165e32c2cddfd584d27
Saikat Sikder <popergyes2017@outlook.com>
2024-08-21 03:32:43 +0600

    new code

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	var saikat string = "Saikat"
+	var saikat2 = "Jane"
+	x := 2
+
+	fmt.Println(saikat)
+	fmt.Println(saikat2)
+	fmt.Println(x)
+
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var d, e, f int = 1, 2, 3
+
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+
+	var g, h = 6, "Hello"
+	i, j := 7, "World!"
+
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+
+	var (
+		ab int
+		bc int    = 1
+		cd string = "hello"
+	)
+	fmt.Println(ab)
+	fmt.Println(bc)
+	fmt.Println(cd)
+
+	const PI = 3.14
+
+	fmt.Println(PI)
+
+}
